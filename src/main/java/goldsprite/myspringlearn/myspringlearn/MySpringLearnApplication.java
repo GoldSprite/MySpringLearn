7b2c1382693011a4e2ab9c5b9c11905b5059dcdc
shikeik <30600841+shikeik@users.noreply.github.com>
2024-03-06 08:18:13 +0800

    初始化项目.

---
+++ src/main/java/goldsprite/myspringlearn/myspringlearn/MySpringLearnApplication.java
@@ -0,0 +1,13 @@
+package goldsprite.myspringlearn.myspringlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/goldsprite/myspringlearn/myspringlearn/MySpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package goldsprite.myspringlearn.myspringlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
